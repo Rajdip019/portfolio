02791099ecb3a694ed78389cc3cb9008907a197b
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2024-07-29 16:15:50 +0530

    Added animation in work experience page

--- components/Expericence/WorkExperience.tsx
+++ components/Expericence/WorkExperience.tsx
@@ -1,14 +1,27 @@
 import { constant } from '@/helpers/constants'
 import React from 'react'
 import WorkExperienceTile from '../Shared/WorkExperienceTile'
+import { motion, useInView } from 'framer-motion';
 
 const WorkExperience = () => {
+
+  const ref = React.useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
-    <div className=' my-10'>
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: isInView ? 1 : 0, y: 0 }}
+      transition={{
+        duration: 0.8,
+        ease: "easeInOut",
+      }}
+      className=' my-10'>
       {constant.workExperience.map((work, index) => (
         <WorkExperienceTile key={index} workExp={work} isCurrent={false} />
       ))}
-    </div>
+    </motion.div>
   )
 }
 
